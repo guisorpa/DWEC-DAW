bab8f592e7f2b81d6219b20148962571d708185f
Guillermo <guillesoriano96@gmail.com>
2017-09-19 20:46:48 +0200

    l

--- Tema1/js/mensaje.js
+++ Tema1/js/mensaje.js
@@ -1,2 +1,6 @@
 alert(t);
 console.log("Esto es una cadena");
+console.info("esto es información");
+console.warn("esto es un aviso");
+console.error("esto es un error");
+document.write("<h1> Lo he cambiado</h1>");
