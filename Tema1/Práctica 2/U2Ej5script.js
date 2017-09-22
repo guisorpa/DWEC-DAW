83bcf4a7f497cc5fc544480149dbba2c7c19f54d
Guillermo <guillesoriano96@gmail.com>
2017-09-22 18:46:36 +0200

    sd

--- Tema1/Práctica 2/U2Ej5script.js
+++ Tema1/Práctica 2/U2Ej5script.js
@@ -1,2 +1,4 @@
-var variable = prompt("Escribe tu color favorito en inglés.")
+var variable = prompt("Escribe tu color favorito en inglés.");
+console.log(variable);
 document.bgColor = variable;
+console.log(document.bgColor);
