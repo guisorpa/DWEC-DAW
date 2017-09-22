eb597dc72b6025323f72a2707082d0ebb287b593
Guillermo <guillesoriano96@gmail.com>
2017-09-22 18:18:51 +0200

    entrega

---
+++ Entrega Prácticas DWEC/Práctica 2/Scripttresycuatro.js
@@ -0,0 +1,2 @@
+console.error("Esto es un error");
+Document.write("<h1>esto es un document.write</h1>");
---
+++ Tema1/Práctica 2/U2Ej5script.js
@@ -0,0 +1,2 @@
+var  variable = prompt("Escribe tu color favorito en inglés.")
+document.bgColor = variable;
