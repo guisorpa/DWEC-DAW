113dbf189d3d19d8b855de527a3ca488e0413940
Guillermo <guillesoriano96@gmail.com>
2017-11-02 19:42:05 +0100

    jj

--- EntregaPracticasDWEC/Practica1Tienda/scriptCarrito.js
+++ EntregaPracticasDWEC/Practica1Tienda/scriptCarrito.js
@@ -1,3 +1,7 @@
 if(navigator.cookieEnabled == "false"){
   alert("TIENES DESHABILITADOS LAS COOKIES. Habilitalas por favor ");
 }
+
+var us1 = new Usuario(producto);
+us1.calcularImporte();
+us1.mostrarComprar();
--- EntregaPracticasDWEC/Practica1Tienda/scriptHome.js
+++ EntregaPracticasDWEC/Practica1Tienda/scriptHome.js
@@ -20,7 +20,7 @@ function verVegano(){
 function condCompraOnline(){
   var v = window.open("", "_blank", "width=400, height=400");
   v.document.write("<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>");
-  v.document.write("<br><button onclick='close()'>Cerrar</button>")
+  v.document.write("<br><button onclick='window.close()'>Cerrar</button>")
 }
 function navegarCarrito(){
   open("carrito.html");
